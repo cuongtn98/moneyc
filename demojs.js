4242340e61dbf598e6a6f541874078877bb791f3
cuongtn98 <cuonghoa1998@gmail.com>
2022-11-08 10:36:53 +0700

    commit

--- demojs.js
+++ demojs.js
@@ -2,15 +2,7 @@ function click1() {
     let chagmo2 = +document.getElementById("chagmo").value;
     let fromc = document.getElementById("wmo").value;
     let toc = document.getElementById("wmo2").value;
+    let changemoney = chagmo2 * toc / fromc ;
+        document.getElementById("lastan").innerHTML=changemoney;
+}
 
-    let result = chagmo2 * 250;
-    let result2 = chagmo2 / 250 ;
-
-
-       if ( fromc == "VND" && toc == "USD") {
-            document.getElementById("lastan").innerHTML=result +"VND";
-
-       }else if (fromc == "USD" && toc =="VND") {
-           document.getElementById("lastan").innerHTML= result2 + "USD"
-       }
-}git
